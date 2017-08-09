aa69c63ea51694681e5c82d02976bffb608e3407
Daniel Smith <l0gr1thm1k@gmail.com>
2017-08-08 19:19:45 -0700

    Add more layer types to network architecture
    - Dense layer
    - Activation layer
    - Dropout layer

--- mla/NeuralNet/layers/basic.py
+++ mla/NeuralNet/layers/basic.py
@@ -9,7 +9,7 @@ np.random.seed(9999)
 
 class Layer(object):
 
-    def setup(self, X_shape):
+    def setup(self, x_shape):
         """Allocates initial weights."""
         pass
 
@@ -67,3 +67,67 @@ class Dense(Layer, ParamMixin):
         if parameters is None:
             self._params = Parameters()
 
+    def setup(self, x_shape):
+        self._params.setup_weights(x_shape[1], self.output_dim)
+
+    def forward_pass(self, x):
+        self.last_input = x
+        return self.weight(x)
+
+    def weight(self, x):
+        weight = np.dot(x, self._params['W'])
+        return weight + self._params['b']
+
+    def backward_pass(self, delta):
+        delta_weight = np.dot(self.last_input.T, delta)
+        delta_bias = np.sum(delta, axis=0)
+
+        # update gradient values
+        self._params.update_grad('W', delta_weight)
+        self._params.update_grad('b', delta_bias)
+        return np.dot(delta, self._params['W'].T)
+
+    def shape(self, x_shape):
+        return x_shape[0], self.output_dim
+
+
+class Activation(Layer):
+
+    def __init__(self, name):
+        self.last_input = None
+        self.activation = get_activation(name)
+        # derivative of activation function
+        self.activation_derivative = elementwise_grad(self.activation)
+
+    def forward_pass(self, x):
+        self.last_input = x
+        return self.activation(x)
+
+    def backward_pass(self, delta):
+        return self.activation_derivative(self.last_input) * delta
+
+    def shape(self, x_shape):
+        return x_shape
+
+
+class Dropout(Layer, PhaseMixin):
+    """Randomly set a number of 'p' inputs to 0 at each training update."""
+
+    def __init__(self, p=0.1):
+        self.p = p
+        self._mask = None
+
+    def forward_pass(self, x):
+        assert self.p > 0
+        if self.is_training:
+            self._mask = np.random.uniform(size=x.shape) > self.p
+            y = x * self._mask
+        else:
+            y = x * (1.0 - self.p)
+        return y
+
+    def backward_pass(self, delta):
+        return delta * self._mask
+
+    def shape(self, x_shape):
+        return x_shape
